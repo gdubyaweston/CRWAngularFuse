214221a97254482abe41f243613309c2bf8205cf
gweston@aictitle.com <gweston@aictitle.com>
2023-08-18 09:22:20 -0500

    11

--- src/app/core/user/user.types.ts
+++ src/app/core/user/user.types.ts
@@ -6,3 +6,12 @@ export interface User
     avatar?: string;
     status?: string;
 }
+
+export interface User1
+{
+    id: string;
+    name: string;
+    email: string;
+    avatar?: string;
+    status?: string;
+}
